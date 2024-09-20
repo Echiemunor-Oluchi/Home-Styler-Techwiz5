0a43a6393911605342f3d9cd30bc5404a7985337
Oluchi Juliet <echiemunoroluchi@gmail.com>
2024-09-20 20:05:58 +0100

    Latest

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,59 @@
 import './components.css';
 import { FaShoppingCart } from "react-icons/fa";
 import { IoPersonSharp } from "react-icons/io5";
-import whitelogo from "../assets/imagefolder/homestyler.jpg"
-import { useEffect } from 'react';
+import whitelogo from "../assets/imagefolder/homestyler.jpg";
+import { useEffect, useState } from 'react';
 import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
+    const handleScroll = () => {
       const navbar = document.querySelector('.header');
       if (window.scrollY > 200) {
-        navbar.classList.add('.change-bg-color');
-}
-  })
-  }, [window.scrollY])
+        navbar.classList.add('change-bg-color');
+      } else {
+        navbar.classList.remove('change-bg-color');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className='header'>
       <div className='navbarHolder'>
-      <div className='navlogo'>
-        <img src={whitelogo} alt="" />
-      </div>
-      <div className='linksHolder'>
-        <ul>
-          <li>Home</li>
-          <li>Design styles</li>
-          <li>Find professionals</li>
-          <li>Products</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Feedback</li>
-        </ul>
-      </div>
-      <div className='navIcons'>
-      <FaShoppingCart />
-      <IoPersonSharp />
-      </div>
-      
-      <div className='hamburger'>
-      <GiHamburgerMenu className='hamburger' />
-      </div>
+        <div className='navlogo'>
+          <img src={whitelogo} alt="logo" />
+        </div>
+        <div className={`linksHolder ${menuOpen ? 'active' : ''}`}>
+          <ul>
+            <li>Home</li>
+            <li>Design styles</li>
+            <li>Find professionals</li>
+            <li>Products</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Feedback</li>
+          </ul>
+        </div>
+        <div className='navIcons'>
+          <FaShoppingCart />
+          <IoPersonSharp />
+        </div>
+
+        <div className='hamburger' onClick={toggleMenu}>
+          <GiHamburgerMenu />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/designstyles/Designstyles.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Designstyles() {
-  return (
-    <div></div>
-  )
-}
-
-export default Designstyles
